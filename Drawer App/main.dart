6a2e916f91bdd461913d930e530f9cd8df8c644f
rameshwari_kodam <rameshwarik2@gmail.com>
2021-04-24 15:12:32 +0530

    added some functionality

--- Drawer App/main.dart
+++ Drawer App/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'members.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       theme: new ThemeData(primarySwatch: Colors.lime),
       home: new HomePage(),
+      //     routes: <String, WidgetBuilder>{
+      //   "/members": (BuildContext context) => new Members("Members List"),
+      // }
     );
   }
 }
@@ -18,19 +22,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Suraj"),
+        title: new Text("Suraj Parivar"),
         // In Iphones there is no elevation
         elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
       ),
       drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
-            new ListTile(
-              title: new Text("Members"),
+            new UserAccountsDrawerHeader(
+              accountName: new Text("Rameshwari Kodam"),
+              accountEmail: new Text("9834278415"),
+              currentAccountPicture: new CircleAvatar(
+                backgroundColor: Colors.brown,
+                child: new Text("R"),
+              ),
             ),
             new ListTile(
-              title: new Text("Din Vishes"),
-            ),
+                title: new Text("Members"),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(
+                    new MaterialPageRoute(
+                      builder: (BuildContext context) => new Members("Member List"),
+                    ),
+                  );
+                }),
+            new ListTile(
+                title: new Text("Din Vishes"),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(
+                    new MaterialPageRoute(
+                      builder: (BuildContext context) => new Members("Member List"),
+                    ),
+                  );
+                }),
             new ListTile(
               title: new Text("Suvichar"),
             ),
@@ -42,6 +68,12 @@ class HomePage extends StatelessWidget {
             ),
             new ListTile(
               title: new Text("Mode"),
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("Close"),
+              trailing: new Icon(Icons.close),
+              onTap: () => Navigator.of(context).pop(),
             ),
           ],
         ),
